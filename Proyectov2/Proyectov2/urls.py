6f181320c02f9013c7d1cbf811e0e0141827a866
Unknown <jorcubas22@gmail.com>
2019-03-19 03:14:06 -0600

    Registro de Usuario terminado.

--- Proyectov2/Proyectov2/urls.py
+++ Proyectov2/Proyectov2/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from proveedor import views as proveedor_views
 from django.contrib.auth import views as auth_views
+from usuario import views as view_usuario
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/',proveedor_views.home,name='home'),
+    path('registro/', view_usuario.registro, name='registro'),
     path('login/', auth_views.LoginView.as_view(template_name='usuario/login.html'), name='login'),
+    path('', include('proveedor.urls')),
 ]
--- Proyectov2/usuario/views.py
+++ Proyectov2/usuario/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
-# Create your views here.
+def registro(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            usuario = form.cleaned_data.get('username')
+            messages.success(request, f'Cuenta Creada Para {usuario}!')
+            return redirect('proveedor-proveedor')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'usuario/registro.html', {'form': form})
+
+
