f70ff6e59a8d8a4ea323615bb762787a92e9110f
Unknown <jorcubas22@gmail.com>
2019-03-18 21:29:13 -0600

    Base Postgresql

--- Proyecto/Proyecto/settings.py
+++ Proyecto/Proyecto/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Proyecto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Proyecto',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
